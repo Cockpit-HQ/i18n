77eb5dae2b6691c59abe0bc48fe7588a0dd778ac
Spyna <iam.lorenzo.spinelli@gmail.com>
2025-09-09 17:37:50 +0200

    Add IT missing translations

---
+++ it/Updater.php
@@ -0,0 +1,10 @@
+<?php return [
+  '@meta' => [
+    'language' => 'Italiano',
+  ],
+  'I acknowledge the risks' => 'Riconosco i rischi',
+  'Settings' => 'Impostazioni',
+  'System Update' => 'Aggiornamento sistema',
+  'Update' => 'Aggiorna',
+  'Your system is up to date.' => 'Il sistema è aggiornato.',
+];
